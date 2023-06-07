93c85c700749f2362aa1d428ee131b775f0e5bab
VishaalStar <135718716+VishaalStar@users.noreply.github.com>
2023-06-07 12:00:28 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+import Textbox from "./textbox"
+
+
+export default function Home() {
+  
+
+  return (
+    <>
+    <div className="mt-4 dark:text-gray-100"><Textbox/></div>
+    
+    
+    </>
+  )
+}
---
+++ app/rsvp/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+
+
+
+export default function Home() {
+  
+
+  return (
+    <>
+    <div className="mt-4 dark:text-gray-100"><h1>rsvp</h1></div>
+    
+    
+    </>
+  )
+}
